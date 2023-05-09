3489e2c497015ce28031a9cb4642851c16acbcfa
Callum Wigg <callumwigg@gmail.com>
2023-05-09 22:30:02 +1000

    removed old scripts

--- batch_transcribe.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-for %%i in (*_norm*.mp3) do (
-    if not exist "%%~ni.txt" (
-        whisper-ctranslate2 "%%i" --model medium.en --language en --condition_on_previous_text False
-    )
-)

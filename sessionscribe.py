ac5c358b0bb89d8282fff7392840771cdd62a37e
CallumWigg <callumwigg@gmail.com>
2023-12-29 13:50:57 +1100

    Fix track num and minor transcription fixes
    Track counting now works for m4a correctly.
    Combined transcription file now shows all files, dates, and track numbers at top of file.

--- sessionscribe.py
+++ sessionscribe.py
@@ -6,11 +6,12 @@ import subprocess
 import sys
 import unicodedata
 
-import enchant
 import taglib
 from faster_whisper import WhisperModel
 from fuzzywuzzy import fuzz, process
 from mutagen.mp3 import MP3
+from mutagen.mp4 import MP4
+from spellchecker import SpellChecker
 
 # File locations
 working_directory = os.path.dirname(os.getcwd())  # Working directory for trawling
@@ -21,12 +22,12 @@ correction_list_file = os.path.join(working_directory, "sessionscribe\\correctio
 def search_audio_files():
     audio_files = []
     current_time = datetime.datetime.now()
-    three_days_ago = current_time - datetime.timedelta(days=3)
+    three_days_ago = current_time - datetime.timedelta(days=7)
 
     # Recursively search for audio files in the directory and its subdirectories
     for root, dirs, files in os.walk(working_directory):
         for file in files:
-            if file.endswith(".wav"):
+            if file.endswith((".wav",".m4a")):
                 file_path = os.path.join(root, file)
                 file_modified_time = datetime.datetime.fromtimestamp(os.path.getmtime(file_path))
                 if file_modified_time >= three_days_ago:
@@ -47,8 +48,8 @@ def get_user_input():
         except ValueError:
             print("Invalid input. Please enter a number.")
 
-# Function to convert an audio file to MP3 format and apply metadata
-def convert_to_mp3(file_path, title, track_number):
+# Function to convert an audio file to m4a format and apply metadata
+def convert_to_m4a(file_path, title):
     # Get the file name and directory separately
     #print(str(file_path))
     input_dir, input_file = os.path.split(file_path)
@@ -58,21 +59,34 @@ def convert_to_mp3(file_path, title, track_number):
     year = datetime.datetime.fromtimestamp(os.path.getmtime(file_path)).year
 
     # Calculate required bitrate for 145MB output file size
-    target_size = 145 * 1024 * 1024
+    target_size = 140 * 1024 * 1024
     target_bitrate = math.floor((target_size * 8) / (input_duration * 1024))
 
-    # Convert to MP3 with calculated bitrate
+    # Convert to M4a with calculated bitrate
     file_name = os.path.splitext(input_file)[0]
     fil_date = file_name[:10]
-    output_file = f"{fil_date}_norm_{file_name[11:]}.mp3"
+    output_file = f"{fil_date}_{file_name[11:]}_norm.m4a"
     output_path = os.path.join(input_dir, output_file)
-    cmd = ['ffmpeg', '-i', file_path, '-af', 'loudnorm', '-c:a', 'libmp3lame', '-b:a', str(target_bitrate)+'k', output_path]
+    cmd = ['ffmpeg', '-i', file_path, '-af', 'loudnorm', '-ac', '1', '-ar', '44100','-c:a', 'aac', '-b:a', str(target_bitrate)+'k', output_path]
     subprocess.run(cmd, check=True)
+
+    # Find the highest track number in existing files
+    existing_files = [f for f in os.listdir(input_dir) if f.endswith('.m4a') and f != output_file]
+    existing_track_numbers = []
+    for f in existing_files:
+        audio = MP4(os.path.join(input_dir, f))
+        track_num = str(audio.get('trkn', [(0,0)])[0][0])
+        if track_num:
+            existing_track_numbers.append(int(track_num))
+    highest_track_number = max(existing_track_numbers) if existing_track_numbers else 0
+
+    # Increment the track number for the new file
+    new_track_number = highest_track_number + 1
 
     # Apply metadata to the audio files, original then new
     with taglib.File(file_path, save_on_exit=True) as song:
         song.tags["TITLE"] = [str(title)]
-        song.tags["TRACKNUMBER"] = [str(track_number)]
+        song.tags["TRACKNUMBER"] = [str(new_track_number)]
         song.tags["ARTIST"] = ["Snek Podcasts"]
         song.tags["GENRE"] = ["Podcast"]
         song.tags["DATE"] = [str(year)]
@@ -80,7 +94,7 @@ def convert_to_mp3(file_path, title, track_number):
 
     with taglib.File(output_path, save_on_exit=True) as song:
         song.tags["TITLE"] = [str(title)]
-        song.tags["TRACKNUMBER"] = [str(track_number)]
+        song.tags["TRACKNUMBER"] = [str(new_track_number)]
         song.tags["ARTIST"] = ["Snek Podcasts"]
         song.tags["GENRE"] = ["Podcast"]
         song.tags["DATE"] = [str(year)]
@@ -94,7 +108,7 @@ def transcribe_audio(input_dir):
     parent_dir = os.path.dirname(os.path.dirname(input_dir))
     transcriptions_folder = next((folder for folder in os.listdir(parent_dir) if os.path.isdir(os.path.join(parent_dir, folder)) and "Transcriptions" in folder), None)
     output_dir = os.path.join(parent_dir, transcriptions_folder) if transcriptions_folder else None
-    subprocess.run(["whisper-ctranslate2", input_dir, "--model", "medium.en", "--language", "en", "--condition_on_previous_text", "False", "--output_dir", output_dir])    
+    subprocess.run(["whisper-ctranslate2", input_dir, "--model", "large-v2", "--language", "en", "--condition_on_previous_text", "False", "--output_dir", output_dir])    
     return os.path.dirname(output_dir)
 
 # Function to combine individual transcriptions into a single mass transcription file
@@ -111,16 +125,19 @@ def transcribe_combine(directory):
 
     campaign = os.path.basename(directory)
 
-    # Create a new Markdown file to write to
-    file_name = campaign + " - Transcriptions.md"
-    output_file = open(os.path.join(directory,file_name), 'w', encoding='utf-8')
+    # Create a new text file to write to
+    file_name = campaign + " - Transcriptions.txt"
+    output_file = open(os.path.join(directory, file_name), 'w', encoding='utf-8')
 
     # Write the campaign information to the output file
     output_file.write('# ' + campaign + '\n\n')
-    output_file.write('**Sessions:** ' + str(len(vtt_files)) + '\n\n')
+    output_file.write('Sessions: ' + str(len(vtt_files)) + '\n\n')
 
-    # Loop through each VTT file and write the contents to the output file
-    for i, vtt_file in enumerate(vtt_files):
+    # Create a list to store the relevant information for each VTT file
+    vtt_info = []
+
+    # Loop through each VTT file and store the relevant information in the list
+    for vtt_file in vtt_files:
         # Extract the date and title from the VTT filename
         file_name = os.path.basename(vtt_file)
         day_str = file_name.split('_')[2].replace('-', '_')
@@ -129,17 +146,40 @@ def transcribe_combine(directory):
         date_str = year_str + '_' + mon_str + '_' + day_str
         date = datetime.datetime.strptime(date_str, '%Y_%m_%d').strftime('%d/%m/%Y')
 
-        # Get the title and track number metadata from the MP3 file using mutagen
+        # Get the title and track number metadata from the m4a file using mutagen
         audio_files_folder = next((folder for folder in os.listdir(directory) if os.path.isdir(os.path.join(directory, folder)) and "Audio Files" in folder), None)
-        mp3_file = os.path.basename(vtt_file.replace('.vtt', '.mp3'))
-        audio = MP3(os.path.join(directory,audio_files_folder,mp3_file))
-        title = str(audio.get('TIT2', ''))
-        track_num = str(audio.get('TRCK', ''))
-        print(date + ' - #' + track_num + ' - ' + title)
-        # Write the header for this section, including track number
-        output_file.write('## ' + title + ' - Session ' + str(track_num) + ' - ' + date + '\n\n')
+        m4a_file = os.path.basename(vtt_file.replace('.vtt', '.m4a'))
+        audio = MP4(os.path.join(directory, audio_files_folder, m4a_file))
+        title = str(audio.get('\xa9nam', [''])[0])
+        track_num = str(audio.get('trkn', [(0, 0)])[0][0])
+
+        # Store the relevant information in a dictionary and append it to the list
+        vtt_info.append({
+            'date': date,
+            'title': title,
+            'track_num': track_num
+        })
+
+    # Loop through the list of VTT file information and write the lines to the output file
+    for info in vtt_info:
+        date = info['date']
+        title = info['title']
+        track_num = info['track_num']
+        output_file.write(date + ' - #' + track_num + ' - ' + title + '\n')
+
+    # Write a newline to separate the campaign info from the VTT file contents
+    output_file.write('\n\n')
+
+    # Loop through each VTT file and write the contents to the output file
+    for vtt_file, info in zip(vtt_files, vtt_info):
+        # Extract metadata as before
+        date = info['date']
+        title = info['title']
+        track_num = info['track_num']
 
         # Read the contents of the VTT file and write to the output file
+        output_file.write('##' + date + ' - #' + track_num + ' - ' + title + '\n')
+
         with open(vtt_file, 'r', encoding='utf-8') as f:
             # Skip the first two lines of the VTT file
             lines = f.readlines()[2:]
@@ -164,13 +204,12 @@ def transcribe_combine(directory):
     output_file.close()
     return output_file.name
 
-# Function to update a dictionary file with new words from the mass transcription file
 def dictionary_update(md_path):
-    # Create a dictionary object using the 'en_US' dictionary
-    dictionary = enchant.Dict("en_AU")
+    # Create a SpellChecker object
+    spell = SpellChecker()
 
     # Open the input file
-    with open(md_path, "r", encoding="utf-8", errors="ignore") as file:
+    with open(md_path, "r", encoding="utf-8") as file:
         text = file.read()
 
     # Define regex pattern for words
@@ -180,30 +219,21 @@ def dictionary_update(md_path):
     words = sorted(set(re.findall(words_pattern, text)))
 
     # Filter out words that are in the standard dictionary
-    non_dict_words = [word for word in words if not dictionary.check(word)]
+    non_dict_words = [word for word in words if not spell.word_frequency[word]]
 
-    # Check if the dictionary file already exists
     try:
-        with open(correction_list_file, "r", encoding="utf-8", errors="ignore") as file:
-            # Read the contents of the file
+        with open(correction_list_file, "r", encoding="utf-8") as file:
             lines = file.readlines()
-
-        # Create a set of words that already have corrections
-        corrected_words = set(line.split(" -> ")[0] for line in lines if "->" in line)
-
-        # Append new words to the end of the file
-        with open(correction_list_file, "a", encoding="utf-8", errors="ignore") as file:
-            for word in sorted(non_dict_words, key=lambda x: x.lower()):
-                # Only write the word to the file if it does not already have a correction
-                if word not in corrected_words:
-                    file.write(f"{word} -> \n")
+            corrected_words = {line.split(" -> ")[0] for line in lines if "->" in line}
     except FileNotFoundError:
-        # Write the results to a new output file with empty columns for corrections
-        with open(correction_list_file, "w", encoding="utf-8", errors="ignore") as file:
-            for word in sorted(non_dict_words, key=lambda x: x.lower()):
+        corrected_words = set()
+
+    with open(correction_list_file, "a", encoding="utf-8") as file:
+        for word in sorted(non_dict_words, key=lambda x: x.lower()):
+            if word not in corrected_words:
                 file.write(f"{word} -> \n")
 
-# Function to perform fuzzy matching and correction on words in the wack_words file
+### Function to perform fuzzy matching and correction on words in the wack_words file
 def fuzzy_fix():
     # Load the list of incorrect words
     with open(correction_list_file, 'r', encoding='utf-8') as f:
@@ -234,7 +264,8 @@ def fuzzy_fix():
         for incorrect, correction in incorrect_words.items():
             f.write(f"{incorrect} -> {correction}\n")
 
-# Function to replace incorrect words in the mass transcription file with corrected versions
+
+#Function to replace incorrect words in the mass transcription file with corrected versions
 def corrections_replace(file_path):
     # Load dictionary
     replacements = {}
@@ -253,7 +284,6 @@ def corrections_replace(file_path):
         for original, replacement in replacements.items():
             pattern = r'\b' + re.escape(original) + r'\b'
             text = re.sub(pattern, replacement, text)
-            print(f'Replacing instance of: "{replacement}"')
 
     # Save output
     with open(file_path, 'w', encoding='utf-8') as f:
@@ -264,13 +294,13 @@ def process_arguments():
     if "--update" in sys.argv and len(sys.argv) >= 3:
         campaign = sys.argv[sys.argv.index("--update") + 1]
         campaign_folder = os.path.join(working_directory,campaign)
-        md_location = transcribe_combine(campaign_folder)
-        print(f'\nstarting dictionary_update at {md_location}')
-        dictionary_update(md_location)
+        txt_location = transcribe_combine(campaign_folder)
+        print(f'\nstarting dictionary_update at {txt_location}')
+        dictionary_update(txt_location)
         print('\nstarting fuzzy_fix')
         fuzzy_fix()
-        print(f'\nstarting corrections_replace at {md_location}')
-        corrections_replace(md_location)
+        print(f'\nstarting corrections_replace at {txt_location}')
+        corrections_replace(txt_location)
         print('\ndone')
     else:
         main()
@@ -286,10 +316,9 @@ def main():
 
     # Prompt the user for metadata: Title, Track Number
     title = input("Enter the title: ")
-    track_number = input("Enter the track number: ")
 
-    # Run the selected file through the convert_to_mp3 function and apply metadata
-    normalised_path = convert_to_mp3(selected_file, title, track_number)
+    # Run the selected file through the convert_to_m4a function and apply metadata
+    normalised_path = convert_to_m4a(selected_file, title)
 
     # Run the converted file through the transcribe_audio function
     campaign_folder = transcribe_audio(normalised_path)
